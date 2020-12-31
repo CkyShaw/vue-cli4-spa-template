f5725a260233ff205138262c0a1d6e2ed0f9e5b9
xieqixuan <Xie_Qixuan@163.com>
2020-12-31 19:17:49 +0800

    1

--- src/components/native-ui/components/chart/index.js
+++
@@ -1,7 +0,0 @@
-import chart from './chart.vue'
-
-chart.install = function (Vue) {
-	Vue.component(chart.name, chart)
-}
-
-export default chart
--- src/components/native-ui/components/high-chart/index.js
+++
@@ -1,7 +0,0 @@
-import highChart from './high-chart.vue'
-
-highChart.install = function (Vue) {
-	Vue.component(highChart.name, highChart)
-}
-
-export default highChart
