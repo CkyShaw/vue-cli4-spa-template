4920099a536405bb2dab200f511ce5059b99f070
xieqixuan <Xie_Qixuan@163.com>
2021-01-04 17:07:49 +0800

    1

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,25 +1,8 @@
 import axios from 'axios'
-// import store from '@/store'
-// import { Spin } from 'view-design'
-const addErrorLog = errorInfo => {
-	const {
-		statusText,
-		status,
-		request: { responseURL }
-	} = errorInfo
-	let info = {
-		type: 'ajax',
-		code: status,
-		mes: statusText,
-		url: responseURL
-	}
-	// if (!responseURL.includes('save_error_logger')) store.dispatch('addErrorLog', info)
-}
 
 class HttpRequest {
 	constructor(baseUrl = baseURL) {
 		this.baseUrl = baseUrl
-		this.queue = {}
 	}
 	getInsideConfig() {
 		const config = {
@@ -30,21 +13,10 @@ class HttpRequest {
 		}
 		return config
 	}
-	destroy(url) {
-		delete this.queue[url]
-		if (!Object.keys(this.queue).length) {
-			// Spin.hide()
-		}
-	}
 	interceptors(instance, url) {
 		// 请求拦截
 		instance.interceptors.request.use(
 			config => {
-				// 添加全局的loading...
-				if (!Object.keys(this.queue).length) {
-					// Spin.show() // 不建议开启，因为界面不友好
-				}
-				this.queue[url] = true
 				return config
 			},
 			error => {
@@ -54,25 +26,10 @@ class HttpRequest {
 		// 响应拦截
 		instance.interceptors.response.use(
 			res => {
-				this.destroy(url)
-				const { data, status } = res
-				return { data, status }
+				const { data } = res
+				return data
 			},
 			error => {
-				this.destroy(url)
-				let errorInfo = error.response
-				if (!errorInfo) {
-					const {
-						request: { statusText, status },
-						config
-					} = JSON.parse(JSON.stringify(error))
-					errorInfo = {
-						statusText,
-						status,
-						request: { responseURL: config.url }
-					}
-				}
-				// addErrorLog(errorInfo)
 				return Promise.reject(error)
 			}
 		)
